fa8511985c05d115fd7e65d761005fbeb7beb880
Clone-facebook <nelsonsimao100@gmail.com>
2022-07-08 18:31:49 +0100

    nova edicão do form do projecto

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import bgIcon from './../../assets/img/bg-copyright.png'
+import './style.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='view-el-content'>
+      <img src={bgIcon} alt="icon" className='iconElement'/>
+        <p className='el-p-item'>© 2022 Grupo create of soft. Todos os direitos reservados. Desenvolvido por Nelson Simão</p>
+    </div>
   )
 }
---
+++ src/components/content/FormElem/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './style.css'
+import bgImg from './../../../assets/img/bg-copyright.png'
+import  ImgUser from './../../../assets/img/user-free-icon-font.png'
+import ImgSMS from './../../../assets/img/envelope-free-icon-font (1).png'
+import ImgWhatsApp from './../../../assets/img/whatsapp-free-icon-font.png'
+import ImgCalendary from './../../../assets/img/calendar-free-icon-font.png'
+
+export default function FormElem() {
+  return (
+    <div className='FormElem'>
+        <img src={bgImg} alt="bg-icon" className='bgImg'/>
+        <form className='Elem-form'>
+            <ul>
+                <li><input type='text' placeholder='Name'/> <img src={ImgUser} alt='icon'/></li>
+                <select name="cars" id="cars">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="opel">Opel</option>
+                    <option value="audi">Audi</option>
+                    
+                </select>
+            </ul>
+            <ul>
+                <li><input type='text' placeholder='E-mail'/> <img src={ImgSMS} alt='icon'/></li>
+                <li><input type='text' placeholder='Abservações'/> <img src={ImgCalendary} alt='icon'/></li>
+            </ul>
+            <ul>
+                <li><input type='text' placeholder='Telefone'/> <img src={ImgWhatsApp} alt='icon'/></li>
+                <input type='submit' value='ENVIAR' className='btn-send'/>
+            </ul>
+        </form>
+    </div>
+  );
+}
