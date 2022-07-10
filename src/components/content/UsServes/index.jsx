f2cfa6b30ae5f42f1361b737024578033c0ae3d4
Clone-facebook <nelsonsimao100@gmail.com>
2022-07-10 17:55:55 +0100

    terminado a edicão da sessão de serviço

--- src/components/content/UsServes/index.jsx
+++ src/components/content/UsServes/index.jsx
@@ -9,9 +9,22 @@ function UsServes() {
             <h1>NOSSOS SERVIÇOS</h1>
             <p>Há mais de 30 anos oferecendo as soluções em segurança que você precisa.</p>
             <div className='servies-el-indetity'>
-            <i class="fa fa-get-pocket fa-2x" aria-hidden="true"></i>
+                <ul>
+                    <li><a src="#">SEGURANÇA PATRIMONIAL</a><i class="fa fa-get-pocket fa-2x" aria-hidden="true"></i></li>
+                    <li><a src="#">SEGURANÇA PESSOAL</a><i class="fa fa-get-pocket fa-2x" aria-hidden="true"></i></li>
+                    <li><a src="#">CONTROLE DE PORTARIA</a><i class="fa fa-get-pocket fa-2x" aria-hidden="true"></i></li>
+                    <li><a src="#">ALARME PERIMETRAL</a><i class="fa fa-get-pocket fa-2x" aria-hidden="true"></i></li>
+                </ul>
+                <ul>
+                    <li><img src={iconSegu} alt="segurança"/></li>
+                </ul>
+                <ul>
+                    <li><i class="fa fa-get-pocket fa-2x" aria-hidden="true"></i><a src="#">MONITORAMENTO DE IMAGENS (CFTV)</a></li>
+                    <li><i class="fa fa-get-pocket fa-2x" aria-hidden="true"></i><a src="#">MONITORAMENTO 24 HORAS</a></li>
+                    <li><i class="fa fa-get-pocket fa-2x" aria-hidden="true"></i><a src="#">SEGURANÇA ELETRÔNICA INTEGRADA</a></li>
+                    <li><i class="fa fa-get-pocket fa-2x" aria-hidden="true"></i><a src="#">PORTARIA VIRTUAL</a></li>
+                </ul>
             </div>
-            <img src={iconSegu} alt="segurança"/>
         </div>
     </>
   );
