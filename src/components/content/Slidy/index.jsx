b96bbfb01beba2db1ac1b038a08cb5d3074bda66
Clone-facebook <nelsonsimao100@gmail.com>
2022-07-10 15:26:54 +0100

    novo push no slidy

--- src/components/content/Slidy/index.jsx
+++ src/components/content/Slidy/index.jsx
@@ -1,50 +1,51 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel'
-import slidy1 from '/home/SrRoot/Documents/gitClone/impresa-de-seguranca/src/assets/img/slidy-1.png'
-import slidy2 from '/home/SrRoot/Documents/gitClone/impresa-de-seguranca/src/assets/img/slidy-2.png'
-import slidy3 from '/home/SrRoot/Documents/gitClone/impresa-de-seguranca/src/assets/img/slidy-3.png'
+import './style.css'
+import slidy1 from './../../../assets/img/slidy-1.png'
+import slidy2 from './../../../assets/img/slidy-2.png'
+import slidy3 from './../../../assets/img/slidy-3.png'
 
 function Slidy() {
   return (
-    <>
+    <div className='langh-slidy'>
         <Carousel fade>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={slidy1}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={slidy2}
-      alt="Second slide"
-    />
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={slidy1}
+            alt="First slide"
+            />
+            <Carousel.Caption>
+            <h3 className='title-el-h3'>First slide label</h3>
+            <p className='sub-el-text'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={slidy2}
+            alt="Second slide"
+            />
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={slidy3}
-      alt="Third slide"
-    />
+            <Carousel.Caption>
+            <h3 className='title-el-h3'>Second slide label</h3>
+            <p className='sub-el-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={slidy3}
+            alt="Third slide"
+            />
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-    </>
+            <Carousel.Caption>
+            <h3 className='title-el-h3'>Third slide label</h3>
+            <p className='sub-el-text'>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        </Carousel>
+    </div>
   )
 }
 
